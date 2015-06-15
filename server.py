e2c0ac2b234d7e1a78e4dcca124f9f5728383e6b
Wenley Tong <wenley.tong@gmail.com>
2015-06-14 17:51:39 -0700

    Working basic templates.

--- server.py
+++ server.py
@@ -6,9 +6,30 @@ app = Flask(__name__)
 # Rendering imports
 from flask import render_template
 
+from talk_db import all_talks
+from compute_distances import distances
+
+# TODO : move into appropriate file
+def edges(talks):
+  d = distances(talks)
+
+  e = []
+  for i in xrange(len(d)):
+    for j in xrange(i + 1, len(d)):
+      e.append({ 'from': i, 'to': j, 'weight': d[i][j]})
+      e.append({ 'from': j, 'to': i, 'weight': d[i][j]})
+  return e
+
+talks = all_talks()
+e = edges(talks)
+
 @app.route('/')
 def main():
-  return render_template('main.html')
+  return render_template('main.html',
+      num_talks=str(len(talks)),
+      talks=talks,
+      num_edges=str(len(e)),
+      edges=e)
 
 if __name__ == "__main__":
   app.run()
